89aeb31df823ced6d0276270c83af1afc3e14a65
KNaresh1 <nareshkakumani@gmail.com>
2023-11-12 15:59:55 -0600

    Add deploy script to deploy NFT smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,33 @@
 const hre = require("hardhat");
 
+const ether = (n) => {
+  return hre.ethers.utils.parseUnits(n.toString(), "ether");
+};
+
 async function main() {
+  const NAME = "Dapp Punks";
+  const SYMBOL = "DP";
+  const COST = ether(10);
+  const MAX_SUPPLY = 25;
+  const NFT_MINT_DATE = (Date.now() + 60000).toString().slice(0, 10); // 1 min from now
+  const IPFS_METADATA_URI =
+    "ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/";
+
   // Fetch contract to deploy
-  const Token = await hre.ethers.getContractFactory("Token");
+  const NFT = await hre.ethers.getContractFactory("NFT");
 
   // Deploy contract
-  const token = await Token.deploy("Dapp Token", "DAPP", 1000000);
-  await token.deployed();
+  let nft = await NFT.deploy(
+    NAME,
+    SYMBOL,
+    COST,
+    MAX_SUPPLY,
+    NFT_MINT_DATE,
+    IPFS_METADATA_URI
+  );
+  await nft.deployed();
 
-  console.log(`Token deployed to: ${token.address}`);
+  console.log(`NFT deployed to: ${nft.address}`);
 }
 
 main().catch((error) => {
