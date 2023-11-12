a1b96dd61c6ac18b2f8ca24902155fdf73d81b09
KNaresh1 <nareshkakumani@gmail.com>
2023-11-12 12:38:56 -0600

    Initial NFT contract changes

---
+++ test/NFT.js
@@ -0,0 +1,66 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+const ether = tokens;
+
+describe("NFT", () => {
+  const NAME = "Dapp Punks";
+  const SYMBOL = "DP";
+  const COST = ether(10);
+  const MAX_SUPPLY = 25;
+  const BASE_URI = "ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/";
+
+  let nft, deployer, minter;
+
+  beforeEach(async () => {
+    [deployer, minter] = await ethers.getSigners();
+  });
+
+  describe("Deployment", () => {
+    const ALLOW_MINTING_ON = (Date.now() + 120000).toString().slice(0, 10); // 2 mins from now
+
+    beforeEach(async () => {
+      const NFT = await ethers.getContractFactory("NFT");
+      nft = await NFT.deploy(
+        NAME,
+        SYMBOL,
+        COST,
+        MAX_SUPPLY,
+        ALLOW_MINTING_ON,
+        BASE_URI
+      );
+    });
+
+    it("has correct name", async () => {
+      expect(await nft.name()).to.equal(NAME);
+    });
+
+    it("has correct symbol", async () => {
+      expect(await nft.symbol()).to.equal(SYMBOL);
+    });
+
+    it("returns cost of nft to mint", async () => {
+      expect(await nft.cost()).to.equal(COST);
+    });
+
+    it("returns the maximum total supply", async () => {
+      expect(await nft.maxSupply()).to.equal(MAX_SUPPLY);
+    });
+
+    it("returns the allowed minting time", async () => {
+      expect(await nft.allowMintingOn()).to.equal(ALLOW_MINTING_ON);
+    });
+
+    it("returns the base uri", async () => {
+      expect(await nft.baseURI()).to.equal(BASE_URI);
+    });
+
+    it("returns the owner", async () => {
+      expect(await nft.owner()).to.equal(deployer.address);
+    });
+  });
+});
